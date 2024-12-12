4ea8b4bd6f03eafbb3c2c76a19acbda9e27a0f6f
IJLAL <ijlal.tanveer294@gmail.com>
2024-12-12 22:08:59 +0500

    updated children prop bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <div className="flex flex-col min-h-screen">
             {/* <Header /> */}
             {/* <main className="flex flex-col flex-1 bg-muted/50">{children}</main> */}
-            <ChatbotPage children={children} />
+            <ChatbotPage>{children}</ChatbotPage>
           </div>
           {/* <ThemeToggle /> */}
         </Providers>
